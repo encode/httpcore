e595acbc3c280a76cea43597ed6063b7057048cf
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2020-03-15 19:28:15 +0000

    Parametrize tests for tunnel and forward

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,9 @@ import pytest
 from mitmproxy import master, options, proxy
 from mitmproxy.tools.dump import DumpMaster
 
+PROXY_HOST = "127.0.0.1"
+PROXY_PORT = 8080
+
 
 @pytest.fixture(
     params=[
@@ -32,31 +35,38 @@ def async_environment(request: typing.Any) -> str:
     return request.param
 
 
+class ProxyWrapper(threading.Thread):
+    def __init__(self, host, port, **kwargs):
+        self.host = host
+        self.port = port
+        self.options = kwargs
+        super().__init__()
+
+    def run(self):
+        # mitmproxy uses asyncio internally but the default loop policy
+        # will only create event loops for the main thread, create one
+        # as part of the thread startup
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        opts = options.Options(
+            listen_host=self.host, listen_port=self.port, **self.options
+        )
+        pconf = proxy.config.ProxyConfig(opts)
+
+        self.master = DumpMaster(opts)
+        self.master.server = proxy.server.ProxyServer(pconf)
+        self.master.run()
+
+    def join(self):
+        self.master.shutdown()
+        super().join()
+
+
 @pytest.fixture
 def proxy_server():
-    host, port = "127.0.0.1", 8080
-
-    class ProxyWrapper(threading.Thread):
-        def run(self):
-            # mitmproxy uses asyncio internally but the default loop policy
-            # will only create event loops for the main thread, create one
-            # as part of the thread startup
-            asyncio.set_event_loop(asyncio.new_event_loop())
-            opts = options.Options(listen_host=host, listen_port=port)
-            pconf = proxy.config.ProxyConfig(opts)
-
-            self.master = DumpMaster(opts)
-            self.master.server = proxy.server.ProxyServer(pconf)
-            self.master.run()
-
-        def join(self):
-            self.master.shutdown()
-            super().join()
-
     try:
-        thread = ProxyWrapper()
+        thread = ProxyWrapper(PROXY_HOST, PROXY_PORT)
         thread.start()
-        time.sleep(2)  # TODO: there's probably a better way to do this
-        yield (host, port)
+        time.sleep(1)  # TODO: there's probably a better way to do this
+        yield (PROXY_HOST, PROXY_PORT)
     finally:
         thread.join()
