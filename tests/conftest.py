d6bf348136c632a1d57bc4c5f81ab0b0b660a127
Yeray Diaz Diaz <yeraydiazdiaz@fastmail.com>
2020-03-21 08:33:10 +0000

    Use mitmproxy as a fixture for proxy tests (#48)
    * POC for using mitmproxy as a fixture
    
    * Comment and cleanup
    
    * Parametrize tests for tunnel and forward
    
    * Add mitmproxy to test requirements
    
    Remove failing tunnel test
    
    * Readd test
    
    Use addon event to notify the proxy server has started
    
    * Return proxy's origin tuple
    
    * Move test to async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
+import asyncio
+import threading
 import typing
 
 import pytest
+from mitmproxy import master, options, proxy
+from mitmproxy.tools.dump import DumpMaster
+
+PROXY_HOST = "127.0.0.1"
+PROXY_PORT = 8080
 
 
 @pytest.fixture(
@@ -25,3 +32,55 @@ def async_environment(request: typing.Any) -> str:
     ```
     """
     return request.param
+
+
+class RunNotify:
+    """A mitmproxy addon wrapping an event to notify us when the server is running."""
+
+    def __init__(self) -> None:
+        self.started = threading.Event()
+
+    def running(self) -> None:
+        self.started.set()
+
+
+class ProxyWrapper(threading.Thread):
+    """Runs an mitmproxy in a separate thread."""
+
+    def __init__(self, host: str, port: int, **kwargs) -> None:
+        self.host = host
+        self.port = port
+        self.options = kwargs
+        super().__init__()
+        self.notify = RunNotify()
+
+    def run(self) -> None:
+        # mitmproxy uses asyncio internally but the default loop policy
+        # will only create event loops for the main thread, create one
+        # as part of the thread startup
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        opts = options.Options(
+            listen_host=self.host, listen_port=self.port, **self.options
+        )
+        pconf = proxy.config.ProxyConfig(opts)
+
+        self.master = DumpMaster(opts)
+        self.master.server = proxy.server.ProxyServer(pconf)
+        self.master.addons.add(self.notify)
+        self.master.run()
+
+    def join(self) -> None:
+        self.master.shutdown()
+        super().join()
+
+
+@pytest.fixture
+def proxy_server() -> typing.Tuple[bytes, bytes, int]:
+    """Starts a proxy server on a different thread and returns its origin tuple."""
+    try:
+        thread = ProxyWrapper(PROXY_HOST, PROXY_PORT)
+        thread.start()
+        thread.notify.started.wait()
+        yield (b"http", PROXY_HOST.encode(), PROXY_PORT)
+    finally:
+        thread.join()
