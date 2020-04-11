a7ce29fb8cb7b3e2a3fafd48bc13d6f4f2764bfe
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2020-04-11 15:24:34 +0100

    Update setup.py
    Co-Authored-By: Florimond Manca <florimond.manca@gmail.com>

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=get_packages("httpcore"),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["h11>=0.8,<0.10", "h2==3.*", "sniffio==1.*",],
+    install_requires=["h11>=0.8,<0.10", "h2==3.*", "sniffio==1.*"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
