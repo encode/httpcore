0538c273eb608d28479eb06c4f7694df51abee51
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2020-04-13 09:03:18 +0100

    Update httpcore/_async/http_proxy.py
    Co-Authored-By: Florimond Manca <florimond.manca@gmail.com>

--- httpcore/_async/http_proxy.py
+++ httpcore/_async/http_proxy.py
@@ -165,7 +165,10 @@ class AsyncHTTPProxy(AsyncConnectionPool):
                 msg = "%d %s" % (proxy_status_code, proxy_reason_phrase.decode("ascii"))
                 raise ProxyError(msg)
 
-            # Create a new connection to the target
+            # The CONNECT request is successful, so we have now SWITCHED PROTOCOLS.
+            # This means the proxy connection is now unusable, and we must create
+            # a new one for regular requests, making sure to use the same socket to
+            # retain the tunnel.
             connection = AsyncHTTPConnection(
                 origin=origin,
                 http2=False,
