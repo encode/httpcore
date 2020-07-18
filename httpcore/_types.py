65ccd0c68b2a26b2a87d82c87f8ba8fa3f22cbf3
florimondmanca <florimond.manca@gmail.com>
2020-07-18 11:40:18 +0200

    Tweak TimeoutDict type variance

--- httpcore/_types.py
+++ httpcore/_types.py
@@ -2,10 +2,10 @@
 Type definitions for type checking purposes.
 """
 
-from typing import Dict, List, Optional, Tuple, Union
+from typing import List, Mapping, Optional, Tuple, Union
 
 StrOrBytes = Union[str, bytes]
 Origin = Tuple[bytes, bytes, int]
 URL = Tuple[bytes, bytes, Optional[int], bytes]
 Headers = List[Tuple[bytes, bytes]]
-TimeoutDict = Dict[str, Optional[float]]
+TimeoutDict = Mapping[str, Optional[float]]
