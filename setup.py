84bd0ddcb8c7c9a37d4db2ad7faaf1e5fb5377c3
Tom Christie <tom@tomchristie.com>
2020-08-03 11:52:09 +0100

    Finalize optional HTTP/2

--- setup.py
+++ setup.py
@@ -53,7 +53,10 @@ setup(
     packages=get_packages("httpcore"),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["h11>=0.8,<0.10", "h2==3.*", "sniffio==1.*"],
+    install_requires=["h11>=0.8,<0.10", "sniffio==1.*"],
+    extras_require={
+        "http2": ["h2==3.*",]
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
