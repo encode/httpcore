27316e646a56a4b72a15f118e04d5e40c98b3baa
Tom Christie <tom@tomchristie.com>
2020-10-06 12:05:56 +0100

    Preserve HTTP header casing

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=get_packages("httpcore"),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["h11>=0.8,<0.11", "sniffio==1.*"],
+    install_requires=["h11==0.*", "sniffio==1.*"],
     extras_require={
         "http2": ["h2>=3,<5"],
     },
