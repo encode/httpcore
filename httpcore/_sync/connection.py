b34bed996eacc4219b79912180f5a51da98fad2c
johnj <10510431+j178@users.noreply.github.com>
2021-06-23 02:26:36 +0800

    Revert "Fix SSLContext warning"
    This reverts commit c02f96b60c0b72fe4b488ee5affa14541ee8c3f8.
    
    # Conflicts:
    #	httpcore/_async/connection.py
    #	httpcore/_async/connection_pool.py
    #	httpcore/_sync/connection.py
    #	httpcore/_sync/connection_pool.py

--- httpcore/_sync/connection.py
+++ httpcore/_sync/connection.py
@@ -1,4 +1,4 @@
-from ssl import PROTOCOL_TLS_CLIENT, SSLContext
+from ssl import SSLContext
 from typing import List, Optional, Tuple, cast
 
 from .._backends.sync import SyncBackend, SyncLock, SyncSocketStream, SyncBackend
@@ -33,9 +33,7 @@ class SyncHTTPConnection(SyncHTTPTransport):
         self._http2_enabled = http2
         self._keepalive_expiry = keepalive_expiry
         self._uds = uds
-        self._ssl_context = (
-            SSLContext(PROTOCOL_TLS_CLIENT) if ssl_context is None else ssl_context
-        )
+        self._ssl_context = SSLContext() if ssl_context is None else ssl_context
         self.socket = socket
         self._local_address = local_address
         self._retries = retries
