abee287c57d2efe5eb2c750e4bf817dddba36869
Tom Christie <tom@tomchristie.com>
2021-11-12 10:40:21 +0000

    Fix import issues. Closes #426 (#428)

--- httpcore/_async/__init__.py
+++ httpcore/_async/__init__.py
@@ -7,7 +7,13 @@ from .interfaces import AsyncConnectionInterface
 try:
     from .http2 import AsyncHTTP2Connection
 except ImportError:  # pragma: nocover
-    pass
+
+    class AsyncHTTP2Connection:  # type: ignore
+        def __init__(self, *args, **kwargs) -> None:  # type: ignore
+            raise RuntimeError(
+                "Attempted to use http2 support, but the `h2` package is not "
+                "installed. Use 'pip install httpcore[http2]'."
+            )
 
 
 __all__ = [
