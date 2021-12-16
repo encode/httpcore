b7c0d8a7c62a9b59e2954559494ca44be2afcf3d
Tom Christie <tom@tomchristie.com>
2021-12-16 14:45:13 +0000

    Only create SSLContext when required (#457)

--- httpcore/_sync/http_proxy.py
+++ httpcore/_sync/http_proxy.py
@@ -3,7 +3,6 @@ from typing import List, Mapping, Optional, Sequence, Tuple, Union
 
 from .._exceptions import ProxyError
 from .._models import URL, Origin, Request, Response, enforce_headers, enforce_url
-from .._ssl import default_ssl_context
 from .._synchronization import Lock
 from ..backends.base import NetworkBackend
 from .connection import HTTPConnection
@@ -80,9 +79,6 @@ class HTTPProxy(ConnectionPool):
             uds: Path to a Unix Domain Socket to use instead of TCP sockets.
             network_backend: A backend instance to use for handling network I/O.
         """
-        if ssl_context is None:
-            ssl_context = default_ssl_context()
-
         super().__init__(
             ssl_context=ssl_context,
             max_connections=max_connections,
@@ -178,7 +174,7 @@ class TunnelHTTPConnection(ConnectionInterface):
         self,
         proxy_origin: Origin,
         remote_origin: Origin,
-        ssl_context: ssl.SSLContext,
+        ssl_context: ssl.SSLContext = None,
         proxy_headers: Sequence[Tuple[bytes, bytes]] = None,
         keepalive_expiry: float = None,
         network_backend: NetworkBackend = None,
