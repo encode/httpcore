2a3f44ab3e5ed81d0815a702609072c9a0d4e06a
Tom Christie <tom@tomchristie.com>
2022-01-03 14:05:00 +0000

    Add tests for proxy HTTP/2 support

--- tests/_sync/test_http_proxy.py
+++ tests/_sync/test_http_proxy.py
@@ -1,3 +1,5 @@
+import hpack
+import hyperframe.frame
 import pytest
 
 from httpcore import HTTPProxy, Origin, ProxyError
@@ -64,7 +66,9 @@ def test_proxy_tunneling():
     """
     network_backend = MockBackend(
         [
+            # The initial response to the proxy CONNECT
             b"HTTP/1.1 200 OK\r\n" b"\r\n",
+            # The actual response from the remote server
             b"HTTP/1.1 200 OK\r\n",
             b"Content-Type: plain/text\r\n",
             b"Content-Length: 13\r\n",
@@ -77,6 +81,72 @@ def test_proxy_tunneling():
         proxy_url="http://localhost:8080/",
         max_connections=10,
         network_backend=network_backend,
+    ) as proxy:
+        # Sending an intial request, which once complete will return to the pool, IDLE.
+        with proxy.stream("GET", "https://example.com/") as response:
+            info = [repr(c) for c in proxy.connections]
+            assert info == [
+                "<TunnelHTTPConnection ['https://example.com:443', HTTP/1.1, ACTIVE, Request Count: 1]>"
+            ]
+            response.read()
+
+        assert response.status == 200
+        assert response.content == b"Hello, world!"
+        info = [repr(c) for c in proxy.connections]
+        assert info == [
+            "<TunnelHTTPConnection ['https://example.com:443', HTTP/1.1, IDLE, Request Count: 1]>"
+        ]
+        assert proxy.connections[0].is_idle()
+        assert proxy.connections[0].is_available()
+        assert not proxy.connections[0].is_closed()
+
+        # A connection on a tunneled proxy can only handle HTTPS requests to the same origin.
+        assert not proxy.connections[0].can_handle_request(
+            Origin(b"http", b"example.com", 80)
+        )
+        assert not proxy.connections[0].can_handle_request(
+            Origin(b"http", b"other.com", 80)
+        )
+        assert proxy.connections[0].can_handle_request(
+            Origin(b"https", b"example.com", 443)
+        )
+        assert not proxy.connections[0].can_handle_request(
+            Origin(b"https", b"other.com", 443)
+        )
+
+
+
+def test_proxy_tunneling_http2():
+    """
+    Send an HTTP/2 request via a proxy.
+    """
+    network_backend = MockBackend(
+        [
+            # The initial response to the proxy CONNECT
+            b"HTTP/1.1 200 OK\r\n" b"\r\n",
+            # The actual response from the remote server
+            hyperframe.frame.SettingsFrame().serialize(),
+            hyperframe.frame.HeadersFrame(
+                stream_id=1,
+                data=hpack.Encoder().encode(
+                    [
+                        (b":status", b"200"),
+                        (b"content-type", b"plain/text"),
+                    ]
+                ),
+                flags=["END_HEADERS"],
+            ).serialize(),
+            hyperframe.frame.DataFrame(
+                stream_id=1, data=b"Hello, world!", flags=["END_STREAM"]
+            ).serialize(),
+        ]
+    )
+
+    with HTTPProxy(
+        proxy_url="http://localhost:8080/",
+        max_connections=10,
+        network_backend=network_backend,
+        http2=True,
     ) as proxy:
         # Sending an intial request, which once complete will return to the pool, IDLE.
         with proxy.stream("GET", "https://example.com/") as response:
