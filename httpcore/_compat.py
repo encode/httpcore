c80846a5cbdc4663990e1944232c7a39694a8ca2
Michael Oliver <michaeloliver__@outlook.com>
2022-03-04 15:38:46 +0000

    Explicitly export asynccontextmanager

--- httpcore/_compat.py
+++ httpcore/_compat.py
@@ -1,6 +1,6 @@
 # `contextlib.asynccontextmanager` exists from Python 3.7 onwards.
 # For 3.6 we require the `async_generator` package for a backported version.
 try:
-    from contextlib import asynccontextmanager
+    from contextlib import asynccontextmanager as asynccontextmanager
 except ImportError:
     from async_generator import asynccontextmanager  # type: ignore[no-redef]  # noqa
