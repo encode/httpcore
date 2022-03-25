6664449ff0c919979f436200555eeb3c989d6985
Michael Adkins <michael@prefect.io>
2022-03-25 10:43:00 -0500

    Bump h11 pin to 0.14

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "h11>=0.11,<=0.13",
+        "h11>=0.11,<0.14",
         "sniffio==1.*",
         "anyio==3.*",
         "certifi",
