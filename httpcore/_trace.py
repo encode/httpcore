4af5e1ced09b6c460251267aea01b65e6f32848d
Tom Christie <tom@tomchristie.com>
2023-03-16 12:14:03 +0000

    Add `DEBUG` level logging. (#648)
    * Add DEBUG level logging
    
    * Add connection.close logging
    
    * Update tests
    
    * Add logging documentation

--- httpcore/_trace.py
+++ httpcore/_trace.py
@@ -1,20 +1,42 @@
+import logging
 from types import TracebackType
 from typing import Any, Dict, Optional, Type
 
 from ._models import Request
 
+logger = logging.getLogger("httpcore")
+
 
 class Trace:
     def __init__(
-        self, name: str, request: Request, kwargs: Optional[Dict[str, Any]] = None
+        self,
+        name: str,
+        request: Optional[Request] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
         self.name = name
-        self.trace = request.extensions.get("trace")
+        self.trace_extension = (
+            None if request is None else request.extensions.get("trace")
+        )
+        self.debug = logger.isEnabledFor(logging.DEBUG)
         self.kwargs = kwargs or {}
         self.return_value: Any = None
+        self.should_trace = self.debug or self.trace_extension is not None
+
+    def trace(self, name: str, info: Dict[str, Any]) -> None:
+        if self.trace_extension is not None:
+            self.trace_extension(name, info)
+
+        if self.debug:
+            if not info or "return_value" in info and info["return_value"] is None:
+                message = name
+            else:
+                args = " ".join([f"{key}={value!r}" for key, value in info.items()])
+                message = f"{name} {args}"
+            logger.debug(message)
 
     def __enter__(self) -> "Trace":
-        if self.trace is not None:
+        if self.should_trace:
             info = self.kwargs
             self.trace(f"{self.name}.started", info)
         return self
@@ -25,7 +47,7 @@ class Trace:
         exc_value: Optional[BaseException] = None,
         traceback: Optional[TracebackType] = None,
     ) -> None:
-        if self.trace is not None:
+        if self.should_trace:
             if exc_value is None:
                 info = {"return_value": self.return_value}
                 self.trace(f"{self.name}.complete", info)
@@ -33,10 +55,22 @@ class Trace:
                 info = {"exception": exc_value}
                 self.trace(f"{self.name}.failed", info)
 
+    async def atrace(self, name: str, info: Dict[str, Any]) -> None:
+        if self.trace_extension is not None:
+            await self.trace_extension(name, info)
+
+        if self.debug:
+            if not info or "return_value" in info and info["return_value"] is None:
+                message = name
+            else:
+                args = " ".join([f"{key}={value!r}" for key, value in info.items()])
+                message = f"{name} {args}"
+            logger.debug(message)
+
     async def __aenter__(self) -> "Trace":
-        if self.trace is not None:
+        if self.should_trace:
             info = self.kwargs
-            await self.trace(f"{self.name}.started", info)
+            await self.atrace(f"{self.name}.started", info)
         return self
 
     async def __aexit__(
@@ -45,10 +79,10 @@ class Trace:
         exc_value: Optional[BaseException] = None,
         traceback: Optional[TracebackType] = None,
     ) -> None:
-        if self.trace is not None:
+        if self.should_trace:
             if exc_value is None:
                 info = {"return_value": self.return_value}
-                await self.trace(f"{self.name}.complete", info)
+                await self.atrace(f"{self.name}.complete", info)
             else:
                 info = {"exception": exc_value}
-                await self.trace(f"{self.name}.failed", info)
+                await self.atrace(f"{self.name}.failed", info)
--- tests/_async/test_connection_pool.py
+++ tests/_async/test_connection_pool.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Optional
 
 import pytest
@@ -157,6 +158,74 @@ async def test_trace_request():
         "http11.receive_response_body.complete",
         "http11.response_closed.started",
         "http11.response_closed.complete",
+    ]
+
+
+@pytest.mark.anyio
+async def test_debug_request(caplog):
+    """
+    The 'trace' request extension allows for a callback function to inspect the
+    internal events that occur while sending a request.
+    """
+    caplog.set_level(logging.DEBUG)
+
+    network_backend = AsyncMockBackend(
+        [
+            b"HTTP/1.1 200 OK\r\n",
+            b"Content-Type: plain/text\r\n",
+            b"Content-Length: 13\r\n",
+            b"\r\n",
+            b"Hello, world!",
+        ]
+    )
+
+    async with AsyncConnectionPool(network_backend=network_backend) as pool:
+        await pool.request("GET", "http://example.com/")
+
+    assert caplog.record_tuples == [
+        (
+            "httpcore",
+            logging.DEBUG,
+            "connection.connect_tcp.started host='example.com' port=80 local_address=None timeout=None",
+        ),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "connection.connect_tcp.complete return_value=<httpcore.AsyncMockStream>",
+        ),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "http11.send_request_headers.started request=<Request [b'GET']>",
+        ),
+        ("httpcore", logging.DEBUG, "http11.send_request_headers.complete"),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "http11.send_request_body.started request=<Request [b'GET']>",
+        ),
+        ("httpcore", logging.DEBUG, "http11.send_request_body.complete"),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "http11.receive_response_headers.started request=<Request [b'GET']>",
+        ),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "http11.receive_response_headers.complete return_value="
+            "(b'HTTP/1.1', 200, b'OK', [(b'Content-Type', b'plain/text'), (b'Content-Length', b'13')])",
+        ),
+        (
+            "httpcore",
+            logging.DEBUG,
+            "http11.receive_response_body.started request=<Request [b'GET']>",
+        ),
+        ("httpcore", logging.DEBUG, "http11.receive_response_body.complete"),
+        ("httpcore", logging.DEBUG, "http11.response_closed.started"),
+        ("httpcore", logging.DEBUG, "http11.response_closed.complete"),
+        ("httpcore", logging.DEBUG, "connection.close.started"),
+        ("httpcore", logging.DEBUG, "connection.close.complete"),
     ]
 
 
