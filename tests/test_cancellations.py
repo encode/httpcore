02166812d9f5de20681ab8fc5d65f2b46c38f2ed
karosis88 <kar.petrosyanpy@gmail.com>
2023-06-15 15:33:35 +0300

    Yet another print fix

--- tests/test_cancellations.py
+++ tests/test_cancellations.py
@@ -93,24 +93,3 @@ async def test_h11_bytestream_cancellation():
                 async for chunk in resp.aiter_stream():
                     pass
         assert conn.is_closed()
-
-
-@pytest.mark.anyio
-async def test_h2_bytestream_cancellation():
-    origin = httpcore.Origin(b"http", b"example.com", 80)
-    stream = SlowReadStream(
-        [
-            b"HTTP/1.1 200 OK\r\n",
-            b"Content-Type: plain/text\r\n",
-            b"Content-Length: 1000\r\n",
-            b"\r\n",
-            b"Hello, world!...",
-        ]
-    )
-    async with httpcore.AsyncHTTP2Connection(origin, stream) as conn:
-        with anyio.move_on_after(132):
-            async with conn.stream("GET", "http://example.com") as resp:
-                print(resp)
-                async for chunk in resp.aiter_stream():
-                    pass
-        assert conn.is_closed()
