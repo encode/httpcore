8cb732417e29a5076a4e186674960d8426f073ee
karosis88 <kar.petrosyanpy@gmail.com>
2023-06-19 16:51:51 +0300

    Add connect, read and write tests for tls connections

--- tests/_backends/test_anyio.py
+++ tests/_backends/test_anyio.py
@@ -1,3 +1,5 @@
+import ssl
+
 import pytest
 
 import httpcore
@@ -33,3 +35,47 @@ async def test_read_without_tls(httpbin):
         await stream.read(1024)
     finally:
         await stream.aclose()
+
+
+@pytest.mark.anyio
+async def test_connect_with_tls(httpbin_secure):
+    backend = httpcore.AnyIOBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = await stream.start_tls(ssl_context=ssl_context)
+    await tls_stream.aclose()
+
+
+@pytest.mark.anyio
+async def test_write_with_tls(httpbin_secure):
+    backend = httpcore.AnyIOBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = await stream.start_tls(ssl_context=ssl_context)
+    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+    try:
+        for chunk in http_request:
+            await tls_stream.write(chunk)
+    finally:
+        await tls_stream.aclose()
+
+
+@pytest.mark.anyio
+async def test_read_with_tls(httpbin_secure):
+    backend = httpcore.AnyIOBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = await stream.start_tls(ssl_context=ssl_context)
+    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+    try:
+        for chunk in http_request:
+            await tls_stream.write(chunk)
+        await tls_stream.read(1024)
+    finally:
+        await tls_stream.aclose()
--- tests/_backends/test_sync.py
+++ tests/_backends/test_sync.py
@@ -1,3 +1,5 @@
+import ssl
+
 import httpcore
 
 
@@ -28,3 +30,44 @@ def test_read_without_tls(httpbin):
         stream.read(1024)
     finally:
         stream.close()
+
+
+def test_connect_with_tls(httpbin_secure):
+    backend = httpcore.SyncBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = stream.start_tls(ssl_context=ssl_context)
+    tls_stream.close()
+
+
+def test_write_with_tls(httpbin_secure):
+    backend = httpcore.SyncBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = stream.start_tls(ssl_context=ssl_context)
+    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+    try:
+        for chunk in http_request:
+            tls_stream.write(chunk)
+    finally:
+        tls_stream.close()
+
+
+def test_read_with_tls(httpbin_secure):
+    backend = httpcore.SyncBackend()
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    tls_stream = stream.start_tls(ssl_context=ssl_context)
+    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+    try:
+        for chunk in http_request:
+            tls_stream.write(chunk)
+        tls_stream.read(1024)
+    finally:
+        tls_stream.close()
