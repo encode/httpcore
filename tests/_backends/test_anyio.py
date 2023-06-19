52b2822c1aa6a2173467f489d34e6e6c889abfdb
karosis88 <kar.petrosyanpy@gmail.com>
2023-06-19 17:26:42 +0300

    Use context manager instead of try/expect

--- tests/_backends/test_anyio.py
+++ tests/_backends/test_anyio.py
@@ -16,12 +16,10 @@ async def test_connect_without_tls(httpbin):
 async def test_write_without_tls(httpbin):
     backend = httpcore.AnyIOBackend()
     stream = await backend.connect_tcp(httpbin.host, httpbin.port)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
+    async with stream:
+        http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
             await stream.write(chunk)
-    finally:
-        await stream.aclose()
 
 
 @pytest.mark.anyio
@@ -29,12 +27,10 @@ async def test_read_without_tls(httpbin):
     backend = httpcore.AnyIOBackend()
     stream = await backend.connect_tcp(httpbin.host, httpbin.port)
     http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
+    async with stream:
         for chunk in http_request:
             await stream.write(chunk)
         await stream.read(1024)
-    finally:
-        await stream.aclose()
 
 
 @pytest.mark.anyio
@@ -44,8 +40,9 @@ async def test_connect_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = await stream.start_tls(ssl_context=ssl_context)
-    await tls_stream.aclose()
+    async with stream:
+        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        await tls_stream.aclose()
 
 
 @pytest.mark.anyio
@@ -55,13 +52,12 @@ async def test_write_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = await stream.start_tls(ssl_context=ssl_context)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
-        for chunk in http_request:
-            await tls_stream.write(chunk)
-    finally:
-        await tls_stream.aclose()
+    async with stream:
+        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        async with tls_stream:
+            http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+            for chunk in http_request:
+                await tls_stream.write(chunk)
 
 
 @pytest.mark.anyio
@@ -71,11 +67,10 @@ async def test_read_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = await stream.start_tls(ssl_context=ssl_context)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
-        for chunk in http_request:
-            await tls_stream.write(chunk)
-        await tls_stream.read(1024)
-    finally:
-        await tls_stream.aclose()
+    async with stream:
+        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        async with tls_stream:
+            http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+            for chunk in http_request:
+                await tls_stream.write(chunk)
+            await tls_stream.read(1024)
--- tests/_backends/test_sync.py
+++ tests/_backends/test_sync.py
@@ -12,24 +12,20 @@ def test_connect_without_tls(httpbin):
 def test_write_without_tls(httpbin):
     backend = httpcore.SyncBackend()
     stream = backend.connect_tcp(httpbin.host, httpbin.port)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
+    with stream:
+        http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
             stream.write(chunk)
-    finally:
-        stream.close()
 
 
 def test_read_without_tls(httpbin):
     backend = httpcore.SyncBackend()
     stream = backend.connect_tcp(httpbin.host, httpbin.port)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
+    with stream:
+        http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
             stream.write(chunk)
         stream.read(1024)
-    finally:
-        stream.close()
 
 
 def test_connect_with_tls(httpbin_secure):
@@ -38,8 +34,9 @@ def test_connect_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = stream.start_tls(ssl_context=ssl_context)
-    tls_stream.close()
+    with stream:
+        tls_stream = stream.start_tls(ssl_context=ssl_context)
+        tls_stream.close()
 
 
 def test_write_with_tls(httpbin_secure):
@@ -48,13 +45,12 @@ def test_write_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = stream.start_tls(ssl_context=ssl_context)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
-        for chunk in http_request:
-            tls_stream.write(chunk)
-    finally:
-        tls_stream.close()
+    with stream:
+        tls_stream = stream.start_tls(ssl_context=ssl_context)
+        with tls_stream:
+            http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+            for chunk in http_request:
+                tls_stream.write(chunk)
 
 
 def test_read_with_tls(httpbin_secure):
@@ -63,11 +59,10 @@ def test_read_with_tls(httpbin_secure):
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
     stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
-    tls_stream = stream.start_tls(ssl_context=ssl_context)
-    http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
-    try:
-        for chunk in http_request:
-            tls_stream.write(chunk)
-        tls_stream.read(1024)
-    finally:
-        tls_stream.close()
+    with stream:
+        tls_stream = stream.start_tls(ssl_context=ssl_context)
+        with tls_stream:
+            http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
+            for chunk in http_request:
+                tls_stream.write(chunk)
+            tls_stream.read(1024)
