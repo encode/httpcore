e31572e0371557d6163d2b7c28676e2b1727673b
karosis88 <kar.petrosyanpy@gmail.com>
2023-06-20 10:58:24 +0300

    Add timeouts for the read/write/connect tests

--- tests/_backends/test_sync.py
+++ tests/_backends/test_sync.py
@@ -4,40 +4,48 @@ from pytest_httpbin import certs  # type: ignore
 
 import httpcore
 
+READ_TIMEOUT = 2
+WRITE_TIMEOUT = 2
+CONNECT_TIMEOUT = 2
+
 
 def test_connect_without_tls(httpbin):
     backend = httpcore.SyncBackend()
-    stream = backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = backend.connect_tcp(httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT)
     stream.close()
 
 
 def test_write_without_tls(httpbin):
     backend = httpcore.SyncBackend()
-    stream = backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = backend.connect_tcp(httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT)
     with stream:
         http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
-            stream.write(chunk)
+            stream.write(chunk, timeout=WRITE_TIMEOUT)
 
 
 def test_read_without_tls(httpbin):
     backend = httpcore.SyncBackend()
-    stream = backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = backend.connect_tcp(httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT)
     with stream:
         http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
-            stream.write(chunk)
-        stream.read(1024)
+            stream.write(chunk, timeout=WRITE_TIMEOUT)
+        stream.read(1024, timeout=READ_TIMEOUT)
 
 
 def test_connect_with_tls(httpbin_secure):
     backend = httpcore.SyncBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     with stream:
         tls_stream = stream.start_tls(
-            ssl_context=ssl_context, server_hostname="localhost"
+            ssl_context=ssl_context,
+            server_hostname="localhost",
+            timeout=CONNECT_TIMEOUT,
         )
         tls_stream.close()
 
@@ -46,28 +54,36 @@ def test_write_with_tls(httpbin_secure):
     backend = httpcore.SyncBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     with stream:
         tls_stream = stream.start_tls(
-            ssl_context=ssl_context, server_hostname="localhost"
+            ssl_context=ssl_context,
+            server_hostname="localhost",
+            timeout=CONNECT_TIMEOUT,
         )
         with tls_stream:
             http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
             for chunk in http_request:
-                tls_stream.write(chunk)
+                tls_stream.write(chunk, timeout=WRITE_TIMEOUT)
 
 
 def test_read_with_tls(httpbin_secure):
     backend = httpcore.SyncBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     with stream:
         tls_stream = stream.start_tls(
-            ssl_context=ssl_context, server_hostname="localhost"
+            ssl_context=ssl_context,
+            server_hostname="localhost",
+            timeout=CONNECT_TIMEOUT,
         )
         with tls_stream:
             http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
             for chunk in http_request:
-                tls_stream.write(chunk)
-            tls_stream.read(1024)
+                tls_stream.write(chunk, timeout=WRITE_TIMEOUT)
+            tls_stream.read(1024, timeout=READ_TIMEOUT)
--- tests/_backends/test_trio.py
+++ tests/_backends/test_trio.py
@@ -5,33 +5,43 @@ from pytest_httpbin import certs  # type: ignore
 
 import httpcore
 
+READ_TIMEOUT = 2
+WRITE_TIMEOUT = 2
+CONNECT_TIMEOUT = 2
+
 
 @pytest.mark.trio
 async def test_connect_without_tls(httpbin):
     backend = httpcore.TrioBackend()
-    stream = await backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = await backend.connect_tcp(
+        httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT
+    )
     await stream.aclose()
 
 
 @pytest.mark.trio
 async def test_write_without_tls(httpbin):
     backend = httpcore.TrioBackend()
-    stream = await backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = await backend.connect_tcp(
+        httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT
+    )
     async with stream:
         http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
         for chunk in http_request:
-            await stream.write(chunk)
+            await stream.write(chunk, timeout=WRITE_TIMEOUT)
 
 
 @pytest.mark.trio
 async def test_read_without_tls(httpbin):
     backend = httpcore.TrioBackend()
-    stream = await backend.connect_tcp(httpbin.host, httpbin.port)
+    stream = await backend.connect_tcp(
+        httpbin.host, httpbin.port, timeout=CONNECT_TIMEOUT
+    )
     http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
     async with stream:
         for chunk in http_request:
-            await stream.write(chunk)
-        await stream.read(1024)
+            await stream.write(chunk, timeout=WRITE_TIMEOUT)
+        await stream.read(1024, timeout=READ_TIMEOUT)
 
 
 @pytest.mark.trio
@@ -39,9 +49,13 @@ async def test_connect_with_tls(httpbin_secure):
     backend = httpcore.TrioBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = await backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     async with stream:
-        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        tls_stream = await stream.start_tls(
+            ssl_context=ssl_context, timeout=CONNECT_TIMEOUT
+        )
         await tls_stream.aclose()
 
 
@@ -50,13 +64,17 @@ async def test_write_with_tls(httpbin_secure):
     backend = httpcore.TrioBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = await backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     async with stream:
-        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        tls_stream = await stream.start_tls(
+            ssl_context=ssl_context, timeout=CONNECT_TIMEOUT
+        )
         async with tls_stream:
             http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
             for chunk in http_request:
-                await tls_stream.write(chunk)
+                await tls_stream.write(chunk, timeout=WRITE_TIMEOUT)
 
 
 @pytest.mark.trio
@@ -64,11 +82,15 @@ async def test_read_with_tls(httpbin_secure):
     backend = httpcore.TrioBackend()
     ssl_context = ssl.create_default_context()
     ssl_context.load_verify_locations(certs.where())
-    stream = await backend.connect_tcp(httpbin_secure.host, httpbin_secure.port)
+    stream = await backend.connect_tcp(
+        httpbin_secure.host, httpbin_secure.port, timeout=CONNECT_TIMEOUT
+    )
     async with stream:
-        tls_stream = await stream.start_tls(ssl_context=ssl_context)
+        tls_stream = await stream.start_tls(
+            ssl_context=ssl_context, timeout=CONNECT_TIMEOUT
+        )
         async with tls_stream:
             http_request = [b"GET / HTTP/1.1\r\n", b"\r\n"]
             for chunk in http_request:
-                await tls_stream.write(chunk)
-            await tls_stream.read(1024)
+                await tls_stream.write(chunk, timeout=WRITE_TIMEOUT)
+            await tls_stream.read(1024, timeout=READ_TIMEOUT)
