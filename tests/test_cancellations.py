57bee0f6546e6a524eb00f3fdd60e2f555a67622
Tom Christie <tom@tomchristie.com>
2023-06-26 11:17:52 +0100

    Add failing test cases for cancellations on connection pools

--- tests/test_cancellations.py
+++ tests/test_cancellations.py
@@ -44,6 +44,59 @@ class SlowReadStream(httpcore.AsyncNetworkStream):
         pass
 
 
+class SlowWriteBackend(httpcore.AsyncNetworkBackend):
+    async def connect_tcp(
+        self,
+        host: str,
+        port: int,
+        timeout: typing.Optional[float] = None,
+        local_address: typing.Optional[str] = None,
+        socket_options: typing.Optional[typing.Iterable[httpcore.SOCKET_OPTION]] = None,
+    ) -> httpcore.AsyncNetworkStream:
+        return SlowWriteStream()
+
+
+class SlowReadBackend(httpcore.AsyncNetworkBackend):
+    def __init__(self, buffer: typing.List[bytes]):
+        self._buffer = buffer
+
+    async def connect_tcp(
+        self,
+        host: str,
+        port: int,
+        timeout: typing.Optional[float] = None,
+        local_address: typing.Optional[str] = None,
+        socket_options: typing.Optional[typing.Iterable[httpcore.SOCKET_OPTION]] = None,
+    ) -> httpcore.AsyncNetworkStream:
+        return SlowReadStream(self._buffer)
+
+
+@pytest.mark.anyio
+async def test_connection_pool_timeout_during_request():
+    network_backend = SlowWriteBackend()
+    async with httpcore.AsyncConnectionPool(network_backend=network_backend) as pool:
+        with anyio.move_on_after(0.001):
+            await pool.request("GET", "http://example.com")
+        assert not pool.connections
+
+
+@pytest.mark.anyio
+async def test_connection_pool_timeout_during_response():
+    network_backend = SlowReadBackend(
+        [
+            b"HTTP/1.1 200 OK\r\n",
+            b"Content-Type: plain/text\r\n",
+            b"Content-Length: 1000\r\n",
+            b"\r\n",
+            b"Hello, world!...",
+        ]
+    )
+    async with httpcore.AsyncConnectionPool(network_backend=network_backend) as pool:
+        with anyio.move_on_after(0.001):
+            await pool.request("GET", "http://example.com")
+        assert not pool.connections
+
+
 @pytest.mark.anyio
 async def test_h11_timeout_during_request():
     """
