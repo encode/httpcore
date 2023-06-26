956dbfd7e6dbe9a6837e2f6694613ee6b48beb9f
Tom Christie <tom@tomchristie.com>
2023-06-26 10:55:14 +0100

    Add failing test case for HTTP/1.1 cancellations during response reading

--- tests/test_cancellations.py
+++ tests/test_cancellations.py
@@ -7,23 +7,73 @@ import httpcore
 
 
 class SlowWriteStream(httpcore.AsyncNetworkStream):
+    """
+    A stream that we can use to test cancellations during
+    the request writing.
+    """
+
     async def write(
         self, buffer: bytes, timeout: typing.Optional[float] = None
     ) -> None:
-        await anyio.sleep(2)
+        await anyio.sleep(999)
 
     async def aclose(self) -> None:
         pass
 
 
-@pytest.mark.anyio
-async def test_h11_response_closed():
+class SlowReadStream(httpcore.AsyncNetworkStream):
     """
-    An async timeout on an HTTP/1.1 connection should leave the connection
-    in a neatly closed state.
+    A stream that we can use to test cancellations during
+    the response reading.
+    """
+
+    def __init__(self, buffer: typing.List[bytes]):
+        self._buffer = buffer
+
+    async def write(self, buffer, timeout=None):
+        pass
+
+    async def read(
+        self, max_bytes: int, timeout: typing.Optional[float] = None
+    ) -> bytes:
+        if not self._buffer:
+            await anyio.sleep(999)
+        return self._buffer.pop(0)
+
+    async def aclose(self):
+        pass
+
+
+@pytest.mark.anyio
+async def test_h11_timeout_during_request():
+    """
+    An async timeout on an HTTP/1.1 during the request writing
+    should leave the connection in a neatly closed state.
     """
     origin = httpcore.Origin(b"http", b"example.com", 80)
     stream = SlowWriteStream()
+    async with httpcore.AsyncHTTP11Connection(origin, stream) as conn:
+        with anyio.move_on_after(0.001):
+            await conn.request("GET", "http://example.com")
+        assert conn.is_closed()
+
+
+@pytest.mark.anyio
+async def test_h11_timeout_during_response():
+    """
+    An async timeout on an HTTP/1.1 during the response reading
+    should leave the connection in a neatly closed state.
+    """
+    origin = httpcore.Origin(b"http", b"example.com", 80)
+    stream = SlowReadStream(
+        [
+            b"HTTP/1.1 200 OK\r\n",
+            b"Content-Type: plain/text\r\n",
+            b"Content-Length: 1000\r\n",
+            b"\r\n",
+            b"Hello, world!...",
+        ]
+    )
     async with httpcore.AsyncHTTP11Connection(origin, stream) as conn:
         with anyio.move_on_after(0.001):
             await conn.request("GET", "http://example.com")
