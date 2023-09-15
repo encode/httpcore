958e402f0be2a273978b501fa56951849f63d0bd
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-15 08:37:17 +0300

    Fix tests

--- tests/_async/test_connection.py
+++ tests/_async/test_connection.py
@@ -37,9 +37,9 @@ async def test_http_connection():
     ) as conn:
         assert not conn.is_idle()
         assert not conn.is_closed()
-        assert not conn.is_available()
+        assert conn.is_available()
         assert not conn.has_expired()
-        assert repr(conn) == "<AsyncHTTPConnection [CONNECTING]>"
+        assert repr(conn) == "<AsyncHTTPConnection [NEW CONNECTION]>"
 
         async with conn.stream("GET", "https://example.com/") as response:
             assert (
