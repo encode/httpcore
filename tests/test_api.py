6d8116b50db46831f4ae815e94fb663e80c337cc
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2024-07-13 17:30:37 +0400

    Add failing tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 import httpcore
 
 
@@ -18,3 +20,8 @@ def test_request_with_content(httpbin):
     response = httpcore.request("POST", url, content=b'{"hello":"world"}')
     assert response.status == 200
     assert json.loads(response.content)["json"] == {"hello": "world"}
+
+
+def test_total_timeout(httpbin):
+    with pytest.raises(Exception):
+        httpcore.request("GET", httpbin.url + "/delay/1", timeout=0.1)
