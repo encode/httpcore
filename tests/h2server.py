10d05d4b670a159414e769d6c74d4bc612c19abb
Daniel Reed <nmlorg@gmail.com>
2024-08-28 21:39:11 -0700

    Add a test showing that slow, overlapping requests back each other up in HTTP/2.
    See https://github.com/encode/httpx/discussions/3278.

--- tests/h2server.py
+++ tests/h2server.py
@@ -1,25 +1,40 @@
+import contextlib
+import functools
+import logging
 import socket
+import threading
+import time
 
+import h2.config
 import h2.connection
 import h2.events
-import h2.config
 
-def send_response(conn, event):
+
+def send_response(sock, conn, event):
+    start = time.time()
+    logging.info("Starting %s.", event)
+
+    time.sleep(1)
+
     stream_id = event.stream_id
     conn.send_headers(
         stream_id=stream_id,
-        headers=[
-            (':status', '200'),
-            ('server', 'basic-h2-server/1.0')
-        ],
+        headers=[(":status", "200"), ("server", "basic-h2-server/1.0")],
     )
-    conn.send_data(
-        stream_id=stream_id,
-        data=b'it works!',
-        end_stream=True
-    )
+    data_to_send = conn.data_to_send()
+    if data_to_send:
+        sock.sendall(data_to_send)
 
-def handle(sock):
+    conn.send_data(stream_id=stream_id, data=b"it works!", end_stream=True)
+    data_to_send = conn.data_to_send()
+    if data_to_send:
+        sock.sendall(data_to_send)
+
+    end = time.time()
+    logging.info("Finished %s in %.03fs.", event, end - start)
+
+
+def handle(sock: socket.socket) -> None:
     config = h2.config.H2Configuration(client_side=False)
     conn = h2.connection.H2Connection(config=config)
     conn.initiate_connection()
@@ -28,22 +43,54 @@ def handle(sock):
     while True:
         data = sock.recv(65535)
         if not data:
+            sock.close()
             break
 
         events = conn.receive_data(data)
         for event in events:
             if isinstance(event, h2.events.RequestReceived):
-                send_response(conn, event)
-
-        data_to_send = conn.data_to_send()
-        if data_to_send:
-            sock.sendall(data_to_send)
+                threading.Thread(
+                    target=functools.partial(send_response, sock, conn, event)
+                ).start()
 
 
-sock = socket.socket()
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('0.0.0.0', 8080))
-sock.listen(5)
+class HTTP2Server:
+    def __init__(
+        self, *, host: str = "127.0.0.1", port: int = 0, timeout: float = 0.2
+    ) -> None:
+        self.sock = socket.socket()
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.settimeout(timeout)
+        self.sock.bind((host, port))
+        self.port = self.sock.getsockname()[1]
+        self.sock.listen(5)
 
-while True:
-    handle(sock.accept()[0])
+    def run(self) -> None:
+        while True:
+            try:
+                handle(self.sock.accept()[0])
+            except socket.timeout:
+                pass
+            except OSError:
+                break
+
+
+@contextlib.contextmanager
+def run(**kwargs):
+    server = HTTP2Server(**kwargs)
+    thr = threading.Thread(target=server.run)
+    thr.start()
+    try:
+        yield server
+    finally:
+        server.sock.close()
+        thr.join()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(relativeCreated)5i <%(threadName)s> %(filename)s:%(lineno)s] %(message)s",
+        level=logging.INFO,
+    )
+
+    HTTP2Server(port=8100).run()
