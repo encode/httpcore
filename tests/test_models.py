6ca267804e6864e4350226f876b97a6e304e13fa
Mateusz Litwin <litwinos@gmail.com>
2024-10-25 10:00:12 -0700

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -41,6 +41,16 @@ def test_url_cannot_include_unicode_strings():
     httpcore.URL(scheme=b"https", host=b"www.example.com", target="/☺".encode("utf-8"))
 
 
+def test_url_origin_socks5():
+    url = httpcore.URL("socks5://127.0.0.1")
+    assert url.origin == httpcore.Origin(scheme=b"socks5", host=b"127.0.0.1", port=1080)
+    assert str(url.origin) == "socks5://127.0.0.1:1080"
+
+    url = httpcore.URL("socks5h://127.0.0.1")
+    assert url.origin == httpcore.Origin(scheme=b"socks5h", host=b"127.0.0.1", port=1080)
+    assert str(url.origin) == "socks5h://127.0.0.1:1080"
+
+
 # Request
 
 
